20a14bd9a670f63fafcc77062ddd6162af91b864
Christophe Benz <christophe.benz@cepremap.org>
2017-12-29 23:03:03 +0100

    Implement fetching dataframe from URL (Web UI connector)

--- README.md
+++ README.md
@@ -14,12 +14,16 @@ pip install --editable .
 
 ## Usage
 
-The `fetch_dataframe` function downloads time series from the DB.nomics Web API and exposes them as a [Pandas DataFrame](http://pandas.pydata.org/pandas-docs/stable/generated/pandas.DataFrame.html).
+The `fetch_dataframe` and `fetch_dataframe_from_url` functions download time series from the DB.nomics Web API and expose them as a [Pandas DataFrame](http://pandas.pydata.org/pandas-docs/stable/generated/pandas.DataFrame.html).
+
+The first takes individual parameters for `provider_code`, `dataset_code` and `series_code`, and the second takes an URL corresponding to the `/dataframe` endpoint of the Web API.
+
+The second version is useful especially in conjunction with the Web UI: on a dataset page (ie https://next.nomics.world/Eurostat/rd_p_persage) there is a "Dataframe URL" link which you can copy-paste as an argument. See the examples below.
 
 ```python
-from dbnomics_client import fetch_dataframe
+from dbnomics_client import fetch_dataframe, fetch_dataframe_from_url
 
-# Fetch a single time series
+# Fetch a single time series, individual params version
 fetch_dataframe(
     provider_code='Eurostat',
     dataset_code='rd_p_persage',
@@ -39,6 +43,9 @@ fetch_dataframe(
     provider_code='Eurostat',
     dataset_code='nrg_134m',
 )
+
+# Fetch all time series of a dataset, URL version (to use with the Web UI)
+fetch_dataframe_from_url("https://next.nomics.world/dataframe/Eurostat/rd_p_persage")
 ```
 
 DB.nomics Web API sets a limit over results and allows pagination. This Python client calls the Web API many times if necessary for you. Please be aware that this mechanism can make the `fetch_dataframe` function quite long to respond. You may use the `limit` parameter:
--- dbnomics_client/__init__.py
+++ dbnomics_client/__init__.py
@@ -32,29 +32,30 @@ log = logging.getLogger(__name__)
 
 
 def fetch_dataframe(provider_code, dataset_code, series_code='', api_base_url=default_api_base_url, limit=None):
-    """Download a dataframe from DB.nomics Web API. A dataframe contains many time series.
+    """Download a dataframe from DB.nomics Web API, giving individual parameters. A dataframe contains many time series.
 
     Return a Python Pandas `DataFrame`.
 
     If the number of time series is greater than the limit allowed by the Web API, this function reconstitutes data
     by making many HTTP requests.
     """
-    def fetch_dataframe_page(offset):
-        dataframe_url = api_base_url + \
-            '/dataframe/{}/{}?series_code={}&offset={}'.format(provider_code, dataset_code, series_code, offset)
-        log.debug(dataframe_url)
-        dataframe_json = requests.get(dataframe_url).json()['dataframe']
-        assert dataframe_json['type'] == 'dict', dataframe_json['type']
-        assert dataframe_json['orient'] == 'columns', dataframe_json['orient']
-        assert dataframe_json['offset'] == offset, dataframe_json['offset']
-        return dataframe_json
-
     if api_base_url.endswith('/'):
         api_base_url = api_base_url[:-1]
+    dataframe_url = api_base_url + '/dataframe/{}/{}?series_code={}'.format(provider_code, dataset_code, series_code)
+    return fetch_dataframe_from_url(dataframe_url, limit=limit)
 
+
+def fetch_dataframe_from_url(url, limit=None):
+    """Download a dataframe from DB.nomics Web API, giving an URL. A dataframe contains many time series.
+
+    Return a Python Pandas `DataFrame`.
+
+    If the number of time series is greater than the limit allowed by the Web API, this function reconstitutes data
+    by making many HTTP requests.
+    """
     dataframe_json_data = []
     while True:
-        dataframe_json = fetch_dataframe_page(offset=len(dataframe_json_data))
+        dataframe_json = fetch_dataframe_page(url, offset=len(dataframe_json_data))
         dataframe_json_data.extend(dataframe_json['data'])
         nb_series = len(dataframe_json_data)
         if limit is not None and nb_series >= limit:
@@ -62,5 +63,22 @@ def fetch_dataframe(provider_code, dataset_code, series_code='', api_base_url=de
             break
         if nb_series == dataframe_json['num_found']:
             break
-
     return pd.DataFrame(dataframe_json_data)
+
+
+def fetch_dataframe_page(dataframe_url, offset):
+    page_url = '{}{}offset={}'.format(
+        dataframe_url,
+        '&' if '?' in dataframe_url else '?',
+        offset,
+    )
+    log.debug(page_url)
+    response_json = requests.get(page_url).json()
+    dataframe_json = response_json.get('dataframe')
+    if dataframe_json is None:
+        raise ValueError('Could not find "dataframe" key in response JSON payload. URL = {!r}, received: {!r}'.format(
+            page_url, response_json))
+    assert dataframe_json['type'] == 'dict', dataframe_json['type']
+    assert dataframe_json['orient'] == 'columns', dataframe_json['orient']
+    assert dataframe_json['offset'] == offset, (dataframe_json['offset'], offset)
+    return dataframe_json
